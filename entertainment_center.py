0cb814408f9b1aa438f0f473a1250285fdf5d4be
Tom Cole <colecode.ph@gmail.com>
2017-05-11 13:00:12 +0800

    Add the year the movie was made to the class and display that information on the page. Also changed some colors.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,35 +1,45 @@
 import media
 import fresh_tomatoes
 
+#movie instances
+
 avatar = media.Movie("Avatar",
+					 "2009",
 					 "A marine on an alien planet.",
 					 "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
 					 "https://www.youtube.com/watch?v=v3qvfCPRWG0")	
 
 moon = media.Movie("Moon",
+				   "2009",
 				   "A Man experiences a personal crisis while living on the moon.",
 				   "https://upload.wikimedia.org/wikipedia/en/b/b0/Moon_%282008%29_film_poster.jpg",
 				   "https://www.youtube.com/watch?v=yjevDdT3bRE")
 
 primer = media.Movie("Primer",
+					 "2004",
 					 "A bunch of guys in a garage discover time travel.",
 					 "https://upload.wikimedia.org/wikipedia/en/f/f7/Primer_%282004_film_poster%29.jpg",
 					 "https://www.youtube.com/watch?v=3nj5MMURCm8")
 
 coherence = media.Movie("Coherence",
+						"2013",
 						"High strangeness ensues at a dinner party as a comet passes by.",
 						"https://upload.wikimedia.org/wikipedia/en/9/9d/Coherence_2013_theatrical_poster.jpg",
 						"https://www.youtube.com/watch?v=sEceDz1Rodc")
 
 pi = media.Movie("Pi",
+				 "1998",
 				 "A mathematician discovers some interesting numbers which drive him to the brink.",
 				 "https://upload.wikimedia.org/wikipedia/en/5/5a/Piposter.jpg",
 				 "https://www.youtube.com/watch?v=jo18VIoR2xU")
 
 the_fountain = media.Movie("The Fountain",
+						   "2006",
 					   	   "An unconventional story about love and immortality.",
 					   	   "https://upload.wikimedia.org/wikipedia/en/e/ee/Fountain_poster_1.jpg",
 					       "https://www.youtube.com/watch?v=-4CgGfYkyFE")
+
+#put the movies in a list and create the .html page
 
 movies = [avatar, moon, primer, coherence, pi, the_fountain]
 fresh_tomatoes.open_movies_page(movies)
--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -19,6 +19,7 @@ main_page_head = '''
     <style type="text/css" media="screen">
         body {
             padding-top: 80px;
+            background-color: black;
         }
         #trailer .modal-dialog {
             margin-top: 200px;
@@ -40,7 +41,7 @@ main_page_head = '''
             padding-top: 20px;
         }
         .movie-tile:hover {
-            background-color: #EEE;
+            background-color: DarkGray;
             cursor: pointer;
         }
         .scale-media {
@@ -124,7 +125,7 @@ main_page_content = '''
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+    <h2>{movie_title} - {movie_year}</h2>
 </div>
 '''
 
@@ -144,8 +145,9 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
+            movie_year=movie.year,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
         )
     return content
 
