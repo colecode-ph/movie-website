238666c6fd7cc40c6b7e6704df139df792c07e28
Tom Cole <colecode.ph@gmail.com>
2017-05-13 13:57:02 +0800

    Add docstring to __init__ function

--- media.py
+++ media.py
@@ -2,6 +2,8 @@ class Movie():
     """This class provides a way to store movie related information"""
     def __init__(self, movie_title, movie_year, movie_storyline, poster_image,
                  trailer_youtube):
+        """This function initializes the class instance, and defines the
+        variables which contain the movie related information"""
         self.title = movie_title
         self.year = movie_year
         self.storyline = movie_storyline
